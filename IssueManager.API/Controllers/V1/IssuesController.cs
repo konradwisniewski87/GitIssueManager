089c416de9566f0888e65535511ed7858b685c07
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-04-08 09:29:15 +0200

    Added IssueManager.API

---
+++ IssueManager.API/Controllers/V1/IssuesController.cs
@@ -0,0 +1,41 @@
+﻿using IssueManager.Core.Interfaces;
+using IssueManager.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssueManager.API.Controllers;
+
+[ApiController]
+[Route("api/issues/{provider}/{repo}")]
+public class IssuesController : ControllerBase
+{
+    private readonly IIssueServiceFactory _serviceFactory;
+
+    public IssuesController(IIssueServiceFactory serviceFactory)
+    {
+        _serviceFactory = serviceFactory;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateIssue(string provider, string repo, [FromBody] IssueRequest request)
+    {
+        var service = _serviceFactory.GetService(provider);
+        var response = await service.CreateIssueAsync(repo, request);
+        return Ok(response);
+    }
+
+    [HttpPut("{issueId:int}")]
+    public async Task<IActionResult> UpdateIssue(string provider, string repo, int issueId, [FromBody] IssueRequest request)
+    {
+        var service = _serviceFactory.GetService(provider);
+        var response = await service.UpdateIssueAsync(repo, issueId, request);
+        return Ok(response);
+    }
+
+    [HttpPatch("{issueId:int}/close")]
+    public async Task<IActionResult> CloseIssue(string provider, string repo, int issueId)
+    {
+        var service = _serviceFactory.GetService(provider);
+        await service.CloseIssueAsync(repo, issueId);
+        return NoContent();
+    }
+}
--- IssueManager.API/Program.cs
+++ IssueManager.API/Program.cs
@@ -1,6 +1,26 @@
+using IssueManager.Core.Interfaces;
+using IssueManager.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Registration GitHubIssueService
+builder.Services.AddHttpClient<GitHubIssueService>();
+builder.Services.AddSingleton<GitHubIssueService>(provider =>
+    new GitHubIssueService(
+        provider.GetRequiredService<HttpClient>(),
+        builder.Configuration["GitHub:Token"]!
+    ));
+
+//Registration GitLabIssueService
+builder.Services.AddHttpClient<GitLabIssueService>();
+builder.Services.AddSingleton<GitLabIssueService>(provider =>
+    new GitLabIssueService(
+        provider.GetRequiredService<HttpClient>(),
+        builder.Configuration["GitLab:Token"]!
+    ));
+
+builder.Services.AddSingleton<IIssueServiceFactory, IssueServiceFactory>();
 
 builder.Services.AddControllers();
 
